13a833f0cf32f447396f3e4413904a99b59f5f4d
AdrienMttn <adrien65ktm@gmail.com>
2026-05-17 21:12:07 +0200

    Modification du frontend

--- go.mod
+++
@@ -1,7 +0,0 @@
-module mon-api
-
-go 1.22.6
-
-require github.com/go-telegram/bot v1.19.0
-
-require github.com/joho/godotenv v1.5.1
--- types/Response.go
+++
@@ -1,7 +0,0 @@
-package types
-
-type Response struct {
-	Status int `json:"status"`
-	Message string `json:"message"`
-	Id string `json:"id,omitempty"`
-}
